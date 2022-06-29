7393550ae7394d2b983809a3b6c5c53dfd7a399f
qwe916 <77445491+qwe916@users.noreply.github.com>
2022-06-29 12:13:14 +0900

    [Silver I] Title: RGB거리, Time: 280 ms, Memory: 20748 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] RGB = new int[N][3];
-        int[][] cost = new int[N][3];
+        int[][] RGB = new int[N][3];//비용 입력받을 배열
+        int[][] cost = new int[N][3];//비용 총값을 저장할 배열
         for (int i = 0; i <RGB.length ; i++) {
             for (int j = 0; j <RGB[i].length ; j++) {
                 RGB[i][j] = sc.nextInt();
@@ -14,10 +14,11 @@ public class Main {
         cost[0][1] = RGB[0][1];
         cost[0][2] = RGB[0][2];
         for (int i = 1; i <cost.length ; i++) {
+            //cost[i][R,G,B 중하나] = Min(cost[i-1][이웃 가능한 색1],cost[i-1][이웃 가능한 색2])+비용
             cost[i][0] = Math.min(cost[i - 1][1], cost[i - 1][2]) + RGB[i][0];
             cost[i][1] = Math.min(cost[i - 1][0], cost[i - 1][2]) + RGB[i][1];
             cost[i][2] = Math.min(cost[i - 1][0], cost[i - 1][1]) + RGB[i][2];
         }
-        System.out.println(Math.min(Math.min(cost[N-1][0],cost[N-1][1]),cost[N-1][2]));
+        System.out.println(Math.min(Math.min(cost[N-1][0],cost[N-1][1]),cost[N-1][2]));//마지막 3개의 RGB 비용중 최솟값
     }
 }
