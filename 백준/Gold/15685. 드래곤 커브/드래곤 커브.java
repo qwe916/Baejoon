b1f47c494ab4660c0ac6fa1eece4a4f270c0d8b9
Hong <77445491+qwe916@users.noreply.github.com>
2022-12-29 21:37:23 +0900

    Update 드래곤 커브.java
    주석 추가

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -25,7 +24,7 @@ public class Main {
 
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-                if (map[i][j] && map[i][j + 1] && map[i + 1][j] && map[i + 1][j + 1]) {
+                if (map[i][j] && map[i][j + 1] && map[i + 1][j] && map[i + 1][j + 1]) {//모든 정사각형 꼭지점이 true
                     ans++;
                 }
             }
@@ -38,15 +37,23 @@ public class Main {
     }
 
     public static void dragonCurve(int x, int y, int d, int g) {
-        List<Integer> d_list = new ArrayList<>();
-        d_list.add(d);
+        List<Integer> d_list = new ArrayList<>();//list 생성
+        d_list.add(d);//방향 저장
 
-        for (int i = 1; i <= g; i++) {
+        for (int i = 1; i <= g; i++) {//드래곤 커브 세대 만큼
             for (int j = d_list.size() - 1; j >= 0; j--) {
-                d_list.add((d_list.get(j) + 1) % 4);
+                d_list.add((d_list.get(j) + 1) % 4);//d_list에 있는 방향들을 마지막부터 뽑으면서 반시계 방향으로 한번 돌린 것을 다시 d_list에 추가한다.
             }
         }
-
+        /**
+         * d_list add -> 뒤부터 (d+1)%4 값 추가
+         * 예
+         * 0 방향 3세대
+         * {0}
+         * {0,1}
+         * {0,1,2,1}
+         * {0,1,2,1,2,3,2,1}
+         */
         map[y][x] = true;
         for (Integer direction : d_list) {
             x += dx[direction];
@@ -55,3 +62,4 @@ public class Main {
         }
     }
 }
+
