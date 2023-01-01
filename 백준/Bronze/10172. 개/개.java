24321eab17229123acf56aa82ad162bcba71893e
Hong <77445491+qwe916@users.noreply.github.com>
2023-01-01 14:49:30 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8));
+        bw.write("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
