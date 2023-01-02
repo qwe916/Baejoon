ec49a12e16931c5c534aa3950182cd2e96c9e1ab
Hong <77445491+qwe916@users.noreply.github.com>
2023-01-02 14:34:58 +0900

    Update 랜선 자르기.java
    주석 추가

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -16,7 +16,7 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
             if(arr[i]>max) max = arr[i];
         }
-        max++;
+        max++;//Upper Bound를 해주어야 한다. 왜냐하면 0과 0이 입력(동일한 랜선 길이 값들만 입력) 되었을 경우 min<max 조건이 아니기 때문에 탐색을 수행하지 않는다. 따라서 이를 방지하기 위해 +1을 해주어서 이를 방지한다.
         long min = 0;
         long mid =  0 ;
         while (min < max) {
