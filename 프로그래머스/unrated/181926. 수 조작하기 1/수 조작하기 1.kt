83e727fd7e857464b59524e6db25b6ec2efc2704
Hong <77445491+qwe916@users.noreply.github.com>
2023-05-13 23:20:32 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.08 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(n: Int, control: String): Int {
+        var answer:Int = n
+        for(i in 0..control.length-1){
+            when(control[i]){
+            'w' -> answer +=1
+            's' -> answer -=1
+            'd' -> answer +=10
+            'a' -> answer -=10
+            }
+        }
+        return answer
+    }
+}
