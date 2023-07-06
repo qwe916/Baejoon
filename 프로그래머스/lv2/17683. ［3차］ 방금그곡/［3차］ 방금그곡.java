803ba5af91a164a7cee0a0d5d33127355cca0c31
Hong <77445491+qwe916@users.noreply.github.com>
2023-07-06 21:23:09 +0900

    [level 2] Title: [3차] 방금그곡, Time: 18.55 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,58 @@
+import java.util.* ;
+ 
+class Solution {
+    static HashMap<String, String> dict = new HashMap<>();
+    public String solution(String m, String[] musicinfos) {
+        String answer = "(None)";
+        
+        dict.put("C#", "c"); dict.put("D#", "d"); dict.put("F#", "f");
+        dict.put("G#", "g"); dict.put("A#", "a");
+        m = change(m);
+        
+        int maxTime = 0;
+        for(int i=0; i<musicinfos.length; i++){
+            String[] temp = musicinfos[i].split(",");
+            int time = getTime(temp[0], temp[1]);
+            String music = change(temp[3]);
+            
+           
+            while(music.length()<time){
+                    music += music;
+            }
+            music = music.substring(0,time);
+            
+           
+            if(music.contains(m)){
+                if(maxTime < time){ 
+                    maxTime = time;
+                    answer = temp[2];
+                }
+            }
+            
+        }
+            
+        return answer;
+    }
+    
+    static String change(String str){
+        for(String key : dict.keySet()){
+            str = str.replace(key, dict.get(key));
+        }
+        return str;
+    }
+    
+ 
+    static int getTime(String str1, String str2){
+        String[] time1 = str1.split(":");
+        String[] time2 = str2.split(":");
+        
+        int h1 = Integer.parseInt(time1[0])*60;
+        int h2 = Integer.parseInt(time2[0])*60;
+        
+        int m1 = Integer.parseInt(time1[1]);
+        int m2 = Integer.parseInt(time2[1]);
+        
+        return (h2+m2)-(h1+m1);
+        
+    }
+}
